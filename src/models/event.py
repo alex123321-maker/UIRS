4acab5bcb6d3a2bd6bd9eca41cca67031cae5d7b
alexander <alexander12110@gmail.com>
2024-11-17 21:59:30 +0700

    Refactor db

--- src/models/event.py
+++ src/models/event.py
@@ -10,6 +10,7 @@ class Event(Base):
     name = Column(String, index=True)
     start_datetime = Column(DateTime)
     end_datetime = Column(DateTime)
+    video = Column(String, nullable=True)
 
 
 class PlannedParticipant(Base):
@@ -26,17 +27,34 @@ class PlannedParticipant(Base):
 Event.participants = relationship("PlannedParticipant", order_by=PlannedParticipant.id, back_populates="event")
 
 
-class Visit(Base):
-    __tablename__ = 'visits'
+class VisitInterval(Base):
+    __tablename__ = 'visitinterval'
 
     id = Column(Integer, primary_key=True, index=True)
     event_id = Column(Integer, ForeignKey('events.id'))
-    employee_id = Column(Integer, ForeignKey('employees.id'), nullable=True)
-    visit_datetime = Column(DateTime)
+    start_datetime= Column(DateTime)
+    end_datetime= Column(DateTime)
+
+    photo = Column(String, nullable=True)
+    order = Column(Integer)
+
+    employees = relationship("IntervalEmployee", back_populates="interval")
+
+    event = relationship("Event", back_populates="visit_interval")
+
+
+Event.visit_interval = relationship("VisitInterval", order_by=VisitInterval.order, back_populates="event")
+
+class IntervalEmployee(Base):
+    __tablename__ = 'interval_employee'
+
+    id = Column(Integer, primary_key=True, index=True)
+    employee_id = Column(Integer, ForeignKey('employees.id'))
+    interval_id = Column(Integer, ForeignKey('visitinterval.id'))
     photo = Column(String)
 
-    event = relationship("Event", back_populates="visits")
-    employee = relationship("Employee", back_populates="visits")
+    interval = relationship("VisitInterval", back_populates="employees")
+
+    employee = relationship("Employee", back_populates="visit_intervals")
 
 
-Event.visits = relationship("Visit", order_by=Visit.id, back_populates="event")
