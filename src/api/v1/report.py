7d74ada380440b7c3b434fbaae4d94ca11e3b3e7
alexander <alexander12110@gmail.com>
2024-12-09 01:02:54 +0700

    fix auth for reports

--- src/api/v1/report.py
+++ src/api/v1/report.py
@@ -1,9 +1,13 @@
+from typing import Annotated
+
 from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.api.dependencies.auth import get_current_user
 from src.api.dependencies.database import get_session
 from src.core.constant import FAIL_VALIDATION_MATCHED_EVENT
+from src.schemas.user import UserFromDB
 from src.services.report import fetch_event_statistics
 
 router = APIRouter()
@@ -12,7 +16,9 @@ router = APIRouter()
 @router.get('/event/{event_id}')
 async def get_event_report(
     event_id: int,
-    db: AsyncSession = Depends(get_session)
+    auth_user: Annotated[UserFromDB, Depends(get_current_user)],
+    db: AsyncSession = Depends(get_session),
+
 ):
     try:
         return await fetch_event_statistics(event_id, db)
